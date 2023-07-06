d21016e7125b4c07518f3e71c259cc5a952317ae
Alex Tester <lex.tester93@gmail.com>
2023-07-06 16:24:11 -0400

    Added for loop to loop through the hours 9-17. Created function to keep track of the time and add/remove the current hour displayed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,21 @@ $(document).ready(function(){
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, diaryText);// Store the value in local storage using the id as the key
     }); 
+     // Loop through the numbers from 9 to 17 and then retrieve the value from local storage
+     for (let i = 9; i <= 17; i++) {
+        $("#"+i+" .description").val(localStorage.getItem(i.toString()));
+       }
+    
+      // Function to update the styling of time blocks based on the current hour
+      function timeTracker(){
+          var currentHour = dayjs().hour(); // Get the current hour using day.js
+          
+          // Toggle the classes based on the condition
+          $('.time-block').each(function() {
+              var idTime = parseInt($(this).attr("id"));
+              $(this)
+                .toggleClass("past", idTime < currentHour) // Add or remove the past class
+                .toggleClass("present", idTime === currentHour) // Add or remove the present class
+                .toggleClass("future", idTime > currentHour); // Add or remove the future class
+          })
+      }
